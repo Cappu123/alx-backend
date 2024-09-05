787ef8eaf80e47763e5bd59cb9f099fabfd8fb79
Cappu123 <cappyalemayehu12@gmail.com>
2024-09-05 01:07:30 +0000

    base caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,8 +11,8 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """adds an item to the cache"""
-        if key == None or item == None:
-            pass
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
